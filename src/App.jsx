909bde9e73b51bc1c299d41db1aea1aa0de8d224
anjana732 <anjanasingh1257@gmail.com>
2024-04-21 12:31:57 +0530

    Added Post Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import Header from "./components/Header"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Sidebar from "./components/Sidebar"
 import Footer from "./components/Footer"
+import CreatePost from "./components/CreatePost";
 import "./App.css";
+import PostList from "./components/PostList";
+
 function App() {
 
   return (
@@ -13,6 +16,8 @@ function App() {
       </div>
       <div className="content">
       <Header></Header>
+      <CreatePost></CreatePost>
+      <PostList></PostList>
       <Footer></Footer>
       </div>
      
---
+++ src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post";
+function PostList(){
+    return(
+        <>
+        <Post></Post>
+        <Post></Post>
+        <Post></Post>
+        </>
+    )
+}
+
+export default PostList
